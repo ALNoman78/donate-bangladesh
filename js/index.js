7407dc419ae0742abed4d787a15332b90cf7de43
ALNoman78 <abdullaalno078@gmail.com>
2024-09-23 14:48:26 +0600

    new html page added and some new functionality added

--- js/index.js
+++ js/index.js
@@ -1,103 +1,135 @@
-document.getElementById('first-button').addEventListener('click', function(){
-    const buttonNoakhali = getInputFieldByNumber('donate-noakhali-field');
-    const noakhaliText = getInnerTextToNumber('noakhali-text-field');
-    const totalBalanceOfNoakhali = buttonNoakhali + noakhaliText ;
-    document.getElementById('noakhali-text-field').innerText = totalBalanceOfNoakhali;
-    // console.log(totalBalanceOfNoakhali);
+document.getElementById("first-button").addEventListener("click", function () {
+    const buttonNoakhali = getInputFieldByNumber("donate-noakhali-field");
+    //  || buttonNoakhali > 0   ||buttonNoakhali === ""|| !isNaN(buttonNoakhali)
 
-    const mainBalance = getInnerTextToNumber('main-balance')
+    if (typeof buttonNoakhali !== 'number' || buttonNoakhali < 0 ) {
+        alert("Invalid Input");
+    } else {
+        
+        const noakhaliText = getInnerTextToNumber("noakhali-text-field");
+        const totalBalanceOfNoakhali = buttonNoakhali + noakhaliText;
+        document.getElementById("noakhali-text-field").innerText =
+        totalBalanceOfNoakhali;
+        const mainBalance = getInnerTextToNumber("main-balance");
     const afterDonatedAmount = mainBalance - totalBalanceOfNoakhali;
-    document.getElementById('main-balance').innerText = afterDonatedAmount
+    document.getElementById("main-balance").innerText = afterDonatedAmount;
+    // added with title
+    const title = document.getElementById("donate-noakhali").innerText;
+    console.log(title);
     // create new element
-    const contentHistory = document.getElementById('history-content')
-    const newElement = document.createElement('div')
+    const contentHistory = document.getElementById("history-content");
+    const newElement = document.createElement("div");
     newElement.innerHTML = `
         <div class = "md:w-10/12 mx-auto border border-solid border-[rgba(17,17,17,0.35)] rounded-lg p-6 my-6">
-            <p>${buttonNoakhali} Tk. is Donated for Donate , at Noakhali Bangladesh</p>
+            <p>${buttonNoakhali} Tk. is ${title}</p>
             <p>Date ${new Date()}</p>
             <p></p>
         </div>
-    `
-    contentHistory.append(newElement)
-})
+    `;
+    contentHistory.append(newElement);
+        // return buttonNoakhali;
+    }
+});
 // feni button
 
-document.getElementById('second-btn').addEventListener('click', function (){
-    const buttonFeni = getInputFieldByNumber('donate-flood-refil');
-    const feniText = getInnerTextToNumber('feni-text')
-    const totalBalanceOfFeni = buttonFeni + feniText
-    document.getElementById('feni-text').innerText = totalBalanceOfFeni;
+document.getElementById("second-btn").addEventListener("click", function () {
+    const buttonFeni = getInputFieldByNumber("donate-flood-refil");
 
-    const mainBalance = getInnerTextToNumber('main-balance')
+    // authorized value || buttonFeni < 0
+
+    if (buttonFeni === "" || !isNaN(buttonFeni )) {
+        const feniText = getInnerTextToNumber("feni-text");
+    const totalBalanceOfFeni = buttonFeni + feniText;
+    document.getElementById("feni-text").innerText = totalBalanceOfFeni;
+
+    const mainBalance = getInnerTextToNumber("main-balance");
     const afterDonatedAmount = mainBalance - totalBalanceOfFeni;
-    document.getElementById('main-balance').innerText = afterDonatedAmount
+    document.getElementById("main-balance").innerText = afterDonatedAmount;
+
+    // title added on append child
+
+    const titleTwo = document.getElementById("title-feni").innerText;
 
     // append element
 
-    const contentHistory = document.getElementById('history-content')
-    const newElement = document.createElement('div')
+    const contentHistory = document.getElementById("history-content");
+    const newElement = document.createElement("div");
     newElement.innerHTML = `
         <div class = "md:w-10/12 mx-auto border border-solid border-[rgba(17,17,17,0.35)] rounded-lg p-6 my-6">
-            <p>${buttonFeni} Tk. is Donated for Donate , at Noakhali Bangladesh</p>
+            <p>${buttonFeni} Tk. is ${titleTwo}</p>
             <p>Date ${new Date()}</p>
             <p></p>
         </div>
-    `
-    contentHistory.append(newElement)
-})
+    `;
+    contentHistory.append(newElement);
+    } else {
+        alert("Invalid Input");
+    }
+});
 
 // quota button
 
-document.getElementById('third-btn').addEventListener('click', function(){
-    const buttonQuota = getInputFieldByNumber('donate-quota');
-    const quotaText = getInnerTextToNumber('quata-text')
-    const totalBalanceOfquata = buttonQuota + quotaText;
-    document.getElementById('quata-text').innerText = totalBalanceOfquata;
+document.getElementById("third-btn").addEventListener("click", function () {
+    const buttonQuota = getInputFieldByNumber("donate-quota");
 
-    const mainBalance = getInnerTextToNumber('main-balance')
+    if (buttonQuota === "" || !isNaN(buttonQuota)) {
+    const mainBalance = getInnerTextToNumber("main-balance");
+
+    console.log("noman");
+    const quotaText = getInnerTextToNumber("quata-text");
+    const totalBalanceOfquata = buttonQuota + quotaText;
+    document.getElementById("quata-text").innerText = totalBalanceOfquata;
     const afterDonatedAmount = mainBalance - totalBalanceOfquata;
-    document.getElementById('main-balance').innerText = afterDonatedAmount
+    document.getElementById("main-balance").innerText = afterDonatedAmount;
+    // append title added on
+    const titleThree = document.getElementById("title-quota").innerText;
 
     // append element
 
-    const contentHistory = document.getElementById('history-content')
-    const newElement = document.createElement('div')
+    const contentHistory = document.getElementById("history-content");
+    const newElement = document.createElement("div");
     newElement.innerHTML = `
         <div class = "md:w-10/12 mx-auto border border-solid border-[rgba(17,17,17,0.35)] rounded-lg p-6 my-6">
-            <p>${buttonQuota} Tk. is Donated for Donate , at Noakhali Bangladesh</p>
+            <p>${buttonQuota} Tk. is ${titleThree}</p>
             <p>Date ${new Date()}</p>
             <p></p>
         </div>
-    `
-    contentHistory.append(newElement)
-})
+    `;
+    contentHistory.append(newElement);
+    } else {
+        alert("Invalid Input");
+    }
+});
 
 // history btn
 
-document.getElementById('History-btn').addEventListener('click', function(){
-    const btn = document.getElementById('History-btn')
-    btn.classList.add('bg-[#B4F461]');
-    btn.classList.add('border-none');
+document.getElementById("History-btn").addEventListener("click", function () {
+    const btn = document.getElementById("History-btn");
+    btn.classList.add("bg-[#B4F461]");
+    btn.classList.add("border-none");
     // toggle section
-    const historyTab = document.getElementById('history-content')
-    historyTab.classList.remove('hidden')
-    const mainContent = document.getElementById('main-content')
-    mainContent.classList.add('hidden')
-    const donationBtn = document.getElementById('donation-btn')
-    donationBtn.classList.remove('bg-[#B4F461]');
+    const historyTab = document.getElementById("history-content");
+    historyTab.classList.remove("hidden");
+    const mainContent = document.getElementById("main-content");
+    mainContent.classList.add("hidden");
+    const donationBtn = document.getElementById("donation-btn");
+    donationBtn.classList.remove("bg-[#B4F461]");
     // create element
-
-    
-})
-// donation button
-document.getElementById('donation-btn').addEventListener('click', function(){
-    const btn = document.getElementById('History-btn')
-    btn.classList.remove('bg-[#B4F461]')
-    const donationBtn = document.getElementById('donation-btn')
-    donationBtn.classList.add('bg-[#B4F461]');
+    });
+    // donation button
+    document.getElementById("donation-btn").addEventListener("click", function () {
+    const btn = document.getElementById("History-btn");
+    btn.classList.remove("bg-[#B4F461]");
+    const donationBtn = document.getElementById("donation-btn");
+    donationBtn.classList.add("bg-[#B4F461]");
     // toggle section
-    const historyTab = document.getElementById('history-content')
-    historyTab.classList.add('hidden')
-    const mainContent = document.getElementById('main-content')
-    mainContent.classList.remove('hidden')
+    const historyTab = document.getElementById("history-content");
+    historyTab.classList.add("hidden");
+    const mainContent = document.getElementById("main-content");
+    mainContent.classList.remove("hidden");
+});
+
+
+document.getElementById('nav-button').addEventListener('click', function(){
+    window.location.href = './home.html'
 })
